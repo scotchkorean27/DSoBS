58e5eb7ee0523b9a785498ff1ac3fae6ec86125b
scott <scottlee27@hotmail.com>
2017-11-04 22:33:48 -0400

    webrender

--- src/DSoBS/GameRunner.java
+++ src/DSoBS/GameRunner.java
@@ -1,10 +1,39 @@
 package DSoBS;
 
-public class GameRunner {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+ 
+ 
+public class GameRunner extends Application {
+    private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new ArticleRenderer(),750,500, Color.web("#666970"));
+        stage.setScene(scene);   
+        stage.show();
+    }
+ 
+    public static void main(String[] args){
+        launch(args);
+    }
 }
+
