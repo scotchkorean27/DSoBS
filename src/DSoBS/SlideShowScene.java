e89d1bb23622ae761f9be3bee87eabd8a27b6704
scott <scottlee27@hotmail.com>
2018-10-06 01:49:53 -0700

    Adds some resolution-fixing to image rendering and scales it down to add some bezels

--- src/DSoBS/SlideShowScene.java
+++ src/DSoBS/SlideShowScene.java
@@ -4,14 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 
 public class SlideShowScene{
-	private ImageView iv1;
+	private ImageView imageView;
 	private Scene scene;
 	private int imageIndex;
 	private String[] imagePaths;
@@ -27,35 +26,11 @@ public class SlideShowScene{
 	}
 	
 	public Scene createScene() {
-		Group root = new Group();
-		// load the image
-		//        "Images/alien-226244_1280.jpg"
+		imageView = new ImageView();
 
+		BorderPane root = new BorderPane();
 
-		// simple displays ImageView the image as is
-		iv1 = new ImageView();
-
-		//			// resizes the image to have width of 100 while preserving the ratio and using
-		//			// higher quality filtering method; this ImageView is also cached to
-		//			// improve performance
-		//			ImageView iv2 = new ImageView();
-		//			iv2.setImage(image);
-		//			iv2.setFitWidth(100);
-		//			iv2.setPreserveRatio(true);
-		//			iv2.setSmooth(true);
-		//			iv2.setCache(true);
-		//
-		//			// defines a viewport into the source image (achieving a "zoom" effect) and
-		//			// displays it rotated
-		//			ImageView iv3 = new ImageView();
-		//			iv3.setImage(image);
-		//			Rectangle2D viewportRect = new Rectangle2D(40, 35, 110, 110);
-		//			iv3.setViewport(viewportRect);
-		//			iv3.setRotate(90);
-		HBox box = new HBox();
-		box.getChildren().add(iv1);
-		root.getChildren().add(box);
-
+		root.setCenter(imageView);
 
 		return (new Scene(root, 1280, 720));
 	}
@@ -64,7 +39,9 @@ public class SlideShowScene{
 		try {
 			InputStream inputStream = new FileInputStream(filepath);
 			Image image = new Image(inputStream);
-			iv1.setImage(image);
+			imageView.setImage(image);
+			imageView.setFitWidth(1024);
+			imageView.setFitHeight(576);
 			inputStream.close();
 		} catch (IOException ex) {
 			ex.printStackTrace();
